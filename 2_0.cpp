472bd640ff5452d334bc8de2c9f17d9c348b2687
Nerrim <90411496+Nerrim@users.noreply.github.com>
2024-10-26 10:57:18 +0200

    Create 2_0.cpp

---
+++ 2_0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ilu;
+    cout << "Podaj ilosc uczestnikow: ";
+    cin >> ilu;
+    if(ilu==0) {cout<<"Organizator dostanie wszystko!";}
+    int pizza = 13;
+    if(pizza-ilu == 0 || pizza-ilu < 0) {pizza=pizza+13;}
+    else { 
+        if(pizza%ilu != 0)
+        {int wyn1=pizza%ilu;
+        int wyn2=pizza/ilu;
+        cout<<"\nkazdy uczestnik dostanie:" << wyn2 << "kawalkow, a organizator dostanie: " << wyn1 <<  " kawalkow\n";
+            
+        } 
+        else {
+            int wyn=pizza/ilu; cout<<"\nkazdy uczestnik dostanie:" << wyn <<" kawalkow";}
+            }
+    return 0;
