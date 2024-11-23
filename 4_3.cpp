57023e5c0ee7f49e1d08b53e8af22fd19595c4e9
Nerrim <90411496+Nerrim@users.noreply.github.com>
2024-11-23 12:24:43 +0100

    Update 4_3.cpp

--- 4_3.cpp
+++ 4_3.cpp
@@ -2,67 +2,73 @@
 
 using namespace std;
 
+void wyswietl(int tab[10][10]){
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if(tab[i][j]<10 && tab[i][j] >= 0) {
+                cout << "| " << tab[i][j] << "|  ";
+            }
+            else {
+                cout << "|" << tab[i][j] << "|  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     int tablica[10][10];
     cout << "Tabica : \n";
     //pierwszy wiersz
     for(int i=0; i<10; i++) {
             tablica[i][0] = i;
-            cout <<"|"<< tablica[i][0]<<"|";
     }
-    cout << endl;
+
     //drugi wiersz
     for(int i=0; i<10; i++) {
         tablica[i][1] = i+i;
-        cout <<"|"<< tablica[i][1]<<"|";
     }
     //trzeci wiersz
     for(int i=0; i<10; i++) {
         tablica[i][2] = i*i;
-        cout <<"|"<< tablica[i][2]<<"|";
     }
-    cout << endl;
+
     //czwarty wiersz
     for(int i=0; i<10; i++) {
         tablica[i][3] = 3+i;
-        cout <<"|"<< tablica[i][3]<<"|";
     }
-    cout << endl;
+
     //piąty wiersz
     for(int i=0; i<10; i++) {
         tablica[i][4] = 4-i;
-        cout <<"|"<< tablica[i][4]<<"|";
     }
-    cout << endl;
+
     //szósty wiersz
     for(int i=0; i<10; i++) {
         tablica[i][5] = 0;
-        cout <<"|"<< tablica[i][5]<<"|";
     }
-    cout << endl;
+
      //siódmy wiersz
     for(int i=0; i<10; i++) {
         tablica[i][6] = 0;
-        cout <<"|"<< tablica[i][6]<<"|";
     }
-    cout << endl;
+
     //ósmy wiersz
     for(int i=0; i<10; i++) {
         tablica[i][7] = 0;
-        cout <<"|"<< tablica[i][7]<<"|";
     }
-    cout << endl;
+
     //dziewiąty wiersz
     for(int i=0; i<10; i++) {
         tablica[i][8] = 0;
-        cout <<"|"<< tablica[i][8]<<"|";
     }
-    cout << endl;
+
     //dziesiąty wiersz
     for(int i=0; i<10; i++) {
         tablica[i][9] = 0;
-        cout <<"|"<< tablica[i][9]<<"|";
     }
-    cout << endl;
+
+    wyswietl(tablica);
+
     return 0;
 }
