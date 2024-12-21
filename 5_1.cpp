b9103ac35cacb163d75bda15f0130536bf43fe90
Miłosz Banaszewski <90411496+Nerrim@users.noreply.github.com>
2024-12-21 11:59:47 +0100

    Update 5_1.cpp

--- 5_1.cpp
+++ 5_1.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-return 0;
+int main()
+{
+    int rozmiar, element, indeks;
+    cout << "Jak wielki ma byc ten wektor: \n";
+    cout << "> ";
+    cin >> rozmiar;
+    rozmiar = rozmiar - 1;
+    cout << endl;
+    cout << "prosze wpisac elementy do tego wektora: \n";
+
+    vector <int> wekt(rozmiar);
+
+    for(int i=0; i<=rozmiar; i++) {
+        cout << "> ";
+        cin >> element;
+        wekt[i] = element;
+    }
+
+    element = wekt[0];
+    indeks = 0;
+    for(int j=0; j<=rozmiar; j++) {
+        if(wekt[j]<wekt[j+1]) { element = wekt[j+1]; indeks=j+1; }
+        else { continue; }
+    }
+
+    cout << endl << endl << "Najwiekszym elementem wektora jest: " << element << endl;
+    cout << "Znajduje sie on na indeksie: " << indeks << endl;
+
+    return 0;
 }
