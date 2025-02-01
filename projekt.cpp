160b50b02715af7664959a2cfd2f87155b7c8827
Miłosz Banaszewski <90411496+Nerrim@users.noreply.github.com>
2025-02-01 12:01:18 +0100

    Update projekt.cpp

--- projekt.cpp
+++ projekt.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
+
+void SleepForNumberOfSeconds(const int & numberofSeconds);
 
 int main() {
     char maze[10][20] = {'#','#','#','#','#','#','#','#','#','#','$','#','#','#','#','#','#','#','#','#','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#','#','.','#','#','.','#','#','#','#','#','.','#','.','#','#','#','#','#','.','#','#','.','#','#','.','#','.','.','.','.','.','#','.','#','.','.','.','#','.','#','#','.','#','#','.','#','.','#','#','#','#','#','.','.','.','#','.','#','.','#','#','.','#','#','.','#','.','#','#','#','#','#','.','#','.','#','.','#','.','#','#','.','.','.','.','.','.','.','.','.','.','#','.','#','.','.','.','#','.','#','#','.','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','.','#','#','.','#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#','#','#','#','@','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'};
 
-
-
-
     char u_control;
-    int pos_v=0;
+    int pos_v=1;
     int pos_h=10;
     int old_v;
     int old_h;
     int k=1;
 
     while(maze[9][3]=='@') {
-
-        cout << "Move " << k << endl;
 
         for(int i=0; i<=9; i++) {
             for(int j=0; j<=19; j++) {
@@ -26,32 +24,24 @@ int main() {
             cout << endl;
         }
 
-        cout << "\nMeaning of blocks: [#] - Wall | [@] - Goal | [$] - Start | [.] - Path \n";
-        cout << "\n\nMovement: [L] - move left | [P] - move right | [D] - move down | [G] - move up || [W] - exit \n\n\n";
-        if(k==1){
-            cout << "Your first move! Go down!\n";
-            cout << ">";
-            cin >> u_control;
-            switch(u_control) {
-                case 'd':
-                    old_v = pos_v;
-                    pos_v = pos_v + 1;
-                    if(maze[pos_v][pos_h] == '#' || maze[pos_v][pos_h] == '$') {
-                        pos_v = pos_v - 1;
-                        k--;
-                        break;
-                    }
-                    else {
-                        maze[pos_v][pos_h] = '&';
-                        maze[old_v][pos_h] = '.';
-                    }
-                    break;
-                    //-------------------//
-                default:
-                    k--;
-                    break;
-                }
+        cout<< "\nESCAPE THIS MAZE!\n";
+        SleepForNumberOfSeconds(5);
+        cout << "\033[2J\033[1;1H";
+
+        cout << "Move " << k << endl;
+
+        maze[pos_v][pos_h]='&';
+
+        for(int i=0; i<=9; i++) {
+            for(int j=0; j<=19; j++) {
+            cout << maze[i][j] << " ";
             }
+            cout << endl;
+        }
+
+        cout << "\nMeaning of blocks: [#] - Wall | [@] - Goal | [$] - Start | [.] - Path | [&] - You\n";
+        cout << "\n\nMovement: [L] - move left | [P] - move right | [D] - move down | [G] - move up || [W] - exit \n\n\n";
+
             cout << ">";
             cin >> u_control;
             switch(u_control) {
@@ -127,7 +117,15 @@ int main() {
 
     cout << "\033[2J\033[1;1H";
     cout << " /$$     /$$                                                          /$$\n|  $$   /$$/                                                         | $$\n \  $$ /$$//$$$$$$  /$$   /$$       /$$  /$$  /$$  /$$$$$$  /$$$$$$$ | $$\n  \  $$$$//$$__  $$| $$  | $$      | $$ | $$ | $$ /$$__  $$| $$__  $$| $$\n   \  $$/| $$  \ $$| $$  | $$      | $$ | $$ | $$| $$  \ $$| $$  \ $$|__/\n    | $$ | $$  | $$| $$  | $$      | $$ | $$ | $$| $$  | $$| $$  | $$    \n    | $$ |  $$$$$$/|  $$$$$$/      |  $$$$$/$$$$/|  $$$$$$/| $$  | $$ /$$\n    |__/  \______/  \______/        \_____/\___/  \______/ |__/  |__/|__/\n                                                                         \n";
-
-
     return 0;
+}
+
+void SleepForNumberOfSeconds(const int & numberofSeconds){
+
+    timespec delay = {numberofSeconds,0}; //500,000 = 1/2 milliseconds
+    timespec delayrem;
+
+    nanosleep(&delay, &delayrem);
+
+    return;
 }
